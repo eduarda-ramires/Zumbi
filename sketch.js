5f24c4fde2b4dc64e6e4091919e8882196707cd5
eduarda-ramires <52322684+eduarda-ramires@users.noreply.github.com>
2023-05-01 14:50:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+
+
+function preload(){
+  
+  
+
+}
+
+function setup() {
+
+  
+createCanvas(windowWidth,windowHeight);
+
+//adicione a imagem de fundo
+  
+  
+
+//crie o sprite do jogador
+
+
+
+}
+
+function draw() {
+background(0); 
+
+//mova o jogador para cima e para baixo e torne o jogo compatível com dispositivos móveis usando touches (toques)
+
+
+//libere as balas e mude a imagem do atirador para a posição de tiro quando a tecla espaço for pressionada
+
+
+//o jogador volta à imagem original quando pararmos de pressionar a tecla espaço
+
+drawSprites();
+
+}
